1a7d83bafd78ae09d420c5d4dafe646b2f947ac9
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-19 14:55:51 -0700

    Add business logic, update methods used in functions to manipulate form input.  Add event listener for submit of page.
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
-const vowels = ["a", "e", "i", "o", "u"]
+const vowels = ["a", "e", "i", "o", "u"];
 
 //Code to test for words beginning with an a:
-RegExp.test([a])
+RegExp.test([a]);
 
 //Code to test for words beginning 
+
+("#formsubmit").submit(function (event) {
+  event.preventDefault();
+  let arrayofinput = [];
+  arrayofinput = $("input#userswords").val()
+  const piglatin = arrayofinput.map(function (string) {
+    if (/[aieou]/) {
+      arrayofinput.push("way");
+    }
+    else if (/[^aieou]/i) {
+      arrayofinput.replace(/[aieou]/gi, (charAt(0) + "ay"));
+    }
+    else if (/[qu]/gi) {
+      $(string).slice(+ "quay");
+    }
+
+    else if ("begins with y"); {
+    }
+    return (translatedword);
+  });
+})
+
+console.log(arrayofinput);
