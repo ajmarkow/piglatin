cc100bf363191d699835907df4c7d2d734d38d40
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-19 15:52:37 -0700

    Update scope of variable definitions, restructure passing of user input to variables
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
 const vowels = ["a", "e", "i", "o", "u"];
 
 //Code to test for words beginning with an a:
-RegExp.test([a]);
+//RegExp.test([a]);
 
 //Code to test for words beginning 
+$(document).ready(function () {
+  $("#formsubmit").submit(function (event) {
+    event.preventDefault();
+    let userWord = ($('form#userinput').val());
+    let arrayOfInput = [];
+    arrayOfInput.push(userWord);
+    const piglatin = arrayofinput.map(function (text) {
+      if (/[aieou]/) {
+        arrayOfInput.push("way");
+        arrayOfInput.join((''));
+      }
+      else if (/[^aieou]/i) {
+        arrayOfInput.replace(/[aieou]/gi, (charAt(0) + "ay"));
+      }
+      else if (/[qu]/gi) {
+        $(string).slice(+ "quay");
+      }
 
-("#formsubmit").submit(function (event) {
-  event.preventDefault();
-  let arrayofinput = [];
-  arrayofinput = $("input#userswords").val()
-  const piglatin = arrayofinput.map(function (string) {
-    if (/[aieou]/) {
-      arrayofinput.push("way");
-    }
-    else if (/[^aieou]/i) {
-      arrayofinput.replace(/[aieou]/gi, (charAt(0) + "ay"));
-    }
-    else if (/[qu]/gi) {
-      $(string).slice(+ "quay");
-    }
-
-    else if ("begins with y"); {
-    }
-    return (translatedword);
+      else if ("begins with y"); {
+      }
+      return (text);
+    });
+    $("#translation").show;
+    console.log(arrayOfInput);
   });
-})
-
-console.log(arrayofinput);
+});
